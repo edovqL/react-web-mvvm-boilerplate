5db34e53595d262caece749e957da509529575de
edovqL <alpredo.vandy@gmail.com>
2025-09-14 20:28:50 +0700

    Backup before Vite migration

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <header>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="mt-4 p-4 bg-blue-600 rounded-lg">
+          <p className="text-red-800 font-semibold">
+            Tailwind CSS is working! 🎉
+          </p>
+        </div>
       </header>
     </div>
   );
